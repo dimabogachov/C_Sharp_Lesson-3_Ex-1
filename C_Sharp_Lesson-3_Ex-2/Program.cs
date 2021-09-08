66524d18ff2a83b7514254de4793bc249ed69d6f
Дмитрий Богачев <dima1983bogachov@gmail.com>
2021-09-08 14:31:43 +0300

    вывод телефонного справочника в виде таблицы

---
+++ C_Sharp_Lesson-3_Ex-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lesson_3_Ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PhoneBook = new [,]
+                {
+                    { "Иванов", "Петров", "Сидоров", "Андреев", "Гаврилов"},
+                    { "+7 (901) 111-1111", "+7 (902) 222-2222", "+7 (903) 333-3333", "+7 (904) 444-4444", "+7 (905) 555-5555"}
+                };
+
+            // Запуск цикла по перебору индекса столбцов
+            for (int i = 0; i < PhoneBook.GetLength(1); i++)
+            {
+                // Запуск цикла по перебору индекса строк
+                for (int j = 0; j < PhoneBook.GetLength(0); j++)
+                {
+                    //Вывод значение элемента массива и установка 4-х пробелов
+                    System.Console.Write($" {PhoneBook[j, i]} ");
+                    System.Console.Write("    ");
+
+
+                }
+                System.Console.WriteLine();
+            }
+
+        }
+    }
+}
